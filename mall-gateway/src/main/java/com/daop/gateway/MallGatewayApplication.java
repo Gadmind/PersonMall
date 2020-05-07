b0a0549f013170c7dfc1f9f6254eee7f44912f54
Gadmind <daopudu@sian.com>
2020-05-07 21:24:08 +0800

    商城网关模块

---
+++ mall-gateway/src/main/java/com/daop/gateway/MallGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.daop.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+
+@EnableDiscoveryClient
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class MallGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallGatewayApplication.class, args);
+    }
+
+}
