a4a2baba7c9d05796750c39ab5b49b2a440138f6
Gadmind <daopudu@sina.com>
2020-10-20 13:30:25 +0800

    日常提交

--- mall-member/src/main/java/com/daop/member/MemberApplication.java
+++ mall-member/src/main/java/com/daop/member/MemberApplication.java
@@ -5,6 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+/**
+ * 调用远程服务
+ * 引入open-feign
+ * 编写接口，告诉Springcloud要调用的远程服务
+ * 声明接口的没一个方法都是调用远程服务的哪一个请求
+ * 开启远程调用功能
+ */
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients(basePackages = "com.daop.member.feign")
